fb67d4a87ac2e67b201d1dc071f5858272ab9b5b
LishanChen0916 <leo965380654@gmail.com>
2020-05-15 17:49:45 +0800

    Finish time warping

--- HW2/forward_kinematics_source_code/motion_editor/kinematics/kinematics_time_warper.cc
+++ HW2/forward_kinematics_source_code/motion_editor/kinematics/kinematics_time_warper.cc
@@ -52,10 +52,82 @@ math::SpatialTemporalVector6d_t TimeWarper::ComputeWarpedMotion(
         const TimeWarpHardConstraintColl_t &hard_constraint_coll
         )
 {
-    // TO DO
     *hard_constraint_coll_ = hard_constraint_coll;
+    math::SpatialTemporalVector6d_t whole_motion_sequence_ = *original_motion_sequence_;
+    float frame_per_second = hard_constraint_coll_->at(2).frame_idx / hard_constraint_coll_->at(2).play_second;
+    int32_t warped_desired_catch_ball_frame_idx = frame_per_second * hard_constraint_coll_->at(1).play_second; //150
+    int32_t desired_catch_ball_frame_idx = hard_constraint_coll_->at(1).frame_idx; //160
 
-    return *original_motion_sequence_;
+    float frame_step_before_catch_ball = (float)warped_desired_catch_ball_frame_idx / (float)desired_catch_ball_frame_idx; //0.97
+    float frame_step_after_catch_ball = (float)(hard_constraint_coll_->at(2).frame_idx - warped_desired_catch_ball_frame_idx) /
+        (float)(hard_constraint_coll_->at(2).frame_idx - desired_catch_ball_frame_idx); //1.34
+    
+    for (int frameidx = 0; frameidx < original_motion_sequence_->temporal_size(); frameidx++) {
+        for (int boneidx = 0; boneidx < original_motion_sequence_->spatial_size(); boneidx++) {
+            if (frameidx <= desired_catch_ball_frame_idx) {
+                float slerp_frame_idx = frame_step_before_catch_ball * frameidx;
+                int32_t prev_frame_idx = floorf(slerp_frame_idx);
+                int32_t next_frame_idx = ceilf(slerp_frame_idx);
+
+                math::Quaternion_t prev_Q = math::ComputeQuaternionXyz(
+                    math::ToRadian(original_motion_sequence_->element(boneidx, prev_frame_idx).angular_vector()).x(),
+                    math::ToRadian(original_motion_sequence_->element(boneidx, prev_frame_idx).angular_vector()).y(),
+                    math::ToRadian(original_motion_sequence_->element(boneidx, prev_frame_idx).angular_vector()).z()
+                );
+
+                math::Quaternion_t next_Q = math::ComputeQuaternionXyz(
+                    math::ToRadian(original_motion_sequence_->element(boneidx, next_frame_idx).angular_vector()).x(),
+                    math::ToRadian(original_motion_sequence_->element(boneidx, next_frame_idx).angular_vector()).y(),
+                    math::ToRadian(original_motion_sequence_->element(boneidx, next_frame_idx).angular_vector()).z()
+                );
+
+                math::Quaternion_t::RealScalar slerpRatio = slerp_frame_idx - prev_frame_idx;
+
+                math::Vector6d_t temp = original_motion_sequence_->element(boneidx, frameidx);
+                temp.set_angular_vector(math::ToDegree(
+                    math::ComputeEulerAngleXyz(
+                        math::ComputeRotMat(
+                            math::Slerp(prev_Q, next_Q, slerpRatio)
+                        )
+                    )
+                ));
+                whole_motion_sequence_.set_element(boneidx, frameidx, temp);
+            }
+
+            else {
+                float slerp_frame_idx = warped_desired_catch_ball_frame_idx + 
+                    frame_step_after_catch_ball * (frameidx - desired_catch_ball_frame_idx);
+                int32_t prev_frame_idx = floorf(slerp_frame_idx);
+                int32_t next_frame_idx = ceilf(slerp_frame_idx);
+
+                math::Quaternion_t prev_Q = math::ComputeQuaternionXyz(
+                    math::ToRadian(original_motion_sequence_->element(boneidx, prev_frame_idx).angular_vector()).x(),
+                    math::ToRadian(original_motion_sequence_->element(boneidx, prev_frame_idx).angular_vector()).y(),
+                    math::ToRadian(original_motion_sequence_->element(boneidx, prev_frame_idx).angular_vector()).z()
+                );
+
+                math::Quaternion_t next_Q = math::ComputeQuaternionXyz(
+                    math::ToRadian(original_motion_sequence_->element(boneidx, next_frame_idx).angular_vector()).x(),
+                    math::ToRadian(original_motion_sequence_->element(boneidx, next_frame_idx).angular_vector()).y(),
+                    math::ToRadian(original_motion_sequence_->element(boneidx, next_frame_idx).angular_vector()).z()
+                );
+
+                math::Quaternion_t::RealScalar slerpRatio = slerp_frame_idx - prev_frame_idx;
+
+                math::Vector6d_t temp = original_motion_sequence_->element(boneidx, frameidx);
+                temp.set_angular_vector(math::ToDegree(
+                    math::ComputeEulerAngleXyz(
+                        math::ComputeRotMat(
+                            math::Slerp(prev_Q, next_Q, slerpRatio)
+                        )
+                    )
+                ));
+                whole_motion_sequence_.set_element(boneidx, frameidx, temp);
+            }
+        }
+    }
+
+    return whole_motion_sequence_;
 }
 
 // protected func.
