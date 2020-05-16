c78bd0a6bb1d8419212cb0c7b451bfb722f46925
LishanChen0916 <leo965380654@gmail.com>
2020-05-16 21:51:40 +0800

    Finish FK

--- HW2/forward_kinematics_source_code/motion_editor/kinematics/kinematics_forward_solver.cc
+++ HW2/forward_kinematics_source_code/motion_editor/kinematics/kinematics_forward_solver.cc
@@ -57,8 +57,55 @@ PoseColl_t ForwardSolver::ComputeSkeletonPose(const int32_t frame_idx)
 
 PoseColl_t ForwardSolver::ComputeSkeletonPose(const math::Vector6dColl_t &joint_spatial_pos)
 {
-    // TO DO
-    return helper_fk_->ComputeSkeletonPose(joint_spatial_pos);
+    PoseColl_t pose_collection;
+    Pose root;
+    root.set_start_pos(joint_spatial_pos.at(0).linear_vector());
+    root.set_end_pos(joint_spatial_pos.at(0).linear_vector());
+    root.set_rotation(math::ComputeRotMatXyz(
+        math::ToRadian(skeleton()->bone_ptr(0)->axis)
+        )
+    );
+    pose_collection.push_back(root);
+    
+    for (int boneidx = 1; boneidx < skeleton()->bone_num(); boneidx++) {
+        acclaim::Bone current_bone = *skeleton()->bone_ptr(boneidx);
+
+        math::RotMat3d_t R_asf, R_amc, R_i_0, R_root;
+        R_i_0.setIdentity();
+        R_root = math::ComputeRotMatXyz(
+            math::ToRadian(joint_spatial_pos.at(0).angular_vector())
+        );
+        
+        while (current_bone.idx != 0) {
+            R_asf = math::ToRotMat(current_bone.rot_parent_current).transpose();
+            R_amc = math::ComputeRotMatXyz(
+                math::ToRadian(
+                    joint_spatial_pos.at(current_bone.idx).angular_vector()
+                )
+            );
+            R_i_0 = R_asf * R_amc * R_i_0;
+
+            current_bone = *current_bone.parent;
+        }
+
+        R_i_0 = R_root * R_i_0;
+
+        math::Vector3d_t V_i = skeleton()->bone_ptr(boneidx)->dir * skeleton()->bone_ptr(boneidx)->length;
+        math::Vector3d_t T_i = pose_collection.at(skeleton()->bone_ptr(boneidx)->parent->idx).end_pos();
+        math::Vector3d_t T_i_plus_1 = R_i_0 * V_i + T_i;
+
+        Pose temp;
+        temp.set_start_pos(T_i);
+        temp.set_end_pos(T_i_plus_1);
+        temp.set_rotation(math::ComputeRotMatXyz(
+            math::ToRadian(skeleton()->bone_ptr(boneidx)->axis)
+            )
+        );
+        
+        pose_collection.push_back(temp);
+    }
+       
+    return pose_collection;
 }
 
 // protected func.
