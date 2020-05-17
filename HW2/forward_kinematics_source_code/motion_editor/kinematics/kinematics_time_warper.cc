0aaaa563e637209a912c113d11e6d55aaac86d8f
LishanChen0916 <leo965380654@gmail.com>
2020-05-17 13:19:05 +0800

    Finish time warping

--- HW2/forward_kinematics_source_code/motion_editor/kinematics/kinematics_time_warper.cc
+++ HW2/forward_kinematics_source_code/motion_editor/kinematics/kinematics_time_warper.cc
@@ -55,12 +55,12 @@ math::SpatialTemporalVector6d_t TimeWarper::ComputeWarpedMotion(
     *hard_constraint_coll_ = hard_constraint_coll;
     math::SpatialTemporalVector6d_t whole_motion_sequence_ = *original_motion_sequence_;
     float frame_per_second = hard_constraint_coll_->at(2).frame_idx / hard_constraint_coll_->at(2).play_second;
-    int32_t warped_desired_catch_ball_frame_idx = frame_per_second * hard_constraint_coll_->at(1).play_second; //150
-    int32_t desired_catch_ball_frame_idx = hard_constraint_coll_->at(1).frame_idx; //160
+    int32_t warped_desired_catch_ball_frame_idx = frame_per_second * hard_constraint_coll_->at(1).play_second;
+    int32_t desired_catch_ball_frame_idx = hard_constraint_coll_->at(1).frame_idx;
 
-    float frame_step_before_catch_ball = (float)warped_desired_catch_ball_frame_idx / (float)desired_catch_ball_frame_idx; //0.97
+    float frame_step_before_catch_ball = (float)warped_desired_catch_ball_frame_idx / (float)desired_catch_ball_frame_idx;
     float frame_step_after_catch_ball = (float)(hard_constraint_coll_->at(2).frame_idx - warped_desired_catch_ball_frame_idx) /
-        (float)(hard_constraint_coll_->at(2).frame_idx - desired_catch_ball_frame_idx); //1.34
+        (float)(hard_constraint_coll_->at(2).frame_idx - desired_catch_ball_frame_idx);
     
     for (int frameidx = 0; frameidx < original_motion_sequence_->temporal_size(); frameidx++) {
         for (int boneidx = 0; boneidx < original_motion_sequence_->spatial_size(); boneidx++) {
@@ -127,6 +127,7 @@ math::SpatialTemporalVector6d_t TimeWarper::ComputeWarpedMotion(
 
     return whole_motion_sequence_;
 }
+
 
 // protected func.
 
