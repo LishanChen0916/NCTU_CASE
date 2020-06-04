c421b95b1399dbfdf18f4af6651924881ae4f7a1
LishanChen0916 <leo965380654@gmail.com>
2020-06-04 11:45:53 +0800

    Finish pseudoinverse solver

--- HW3/inverse_kinematics_source_code/motion_editor/math/math_pseudoinverse_solver.cc
+++ HW3/inverse_kinematics_source_code/motion_editor/math/math_pseudoinverse_solver.cc
@@ -27,8 +27,22 @@ math::VectorNd_t PseudoinverseSolver::Solve(
         const math::MatrixN_t &coef_mat,
         const math::VectorNd_t &desired_vector
         ) const
-{//TO DO
-    return math::VectorNd_t();
+{
+    math:VectorNd_t angularVector(coef_mat.cols());
+    math:MatrixN_t JacobianInverse(coef_mat.cols(), coef_mat.rows());
+
+    Eigen::JacobiSVD<math::MatrixN_t> svd(coef_mat, Eigen::ComputeThinU | Eigen::ComputeThinV);
+    Eigen::MatrixXd sigma(3, 3);
+
+    sigma.setIdentity();
+    sigma(0, 0) = svd.singularValues()[0];
+    sigma(1, 1) = svd.singularValues()[1];
+    sigma(2, 2) = svd.singularValues()[2];
+
+    JacobianInverse = svd.matrixV() * sigma.inverse() * svd.matrixU().transpose();
+    angularVector = JacobianInverse * desired_vector;
+
+    return angularVector;
 }
 
 // protected func.
